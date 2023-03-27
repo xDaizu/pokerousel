704d37f1bd3e8e77c819d76e2756c821f995e6bf
Danny Even <danielparejom@gmail.com>
2023-03-27 11:47:50 +0200

    Remove Fairy typing (pokeMMO has no fairy type)

--- src/core/Pokedex.ts
+++ src/core/Pokedex.ts
@@ -1,5 +1,5 @@
 import pokedexDataJSON from '../static/pokedex/pokedex.json'
-import type { Pokemon } from './Pokemon'
+import type {Pokemon} from './Pokemon'
 
 type PokemonListJson = PokemonJson[]
 type PokemonJson = {
@@ -7,6 +7,9 @@ type PokemonJson = {
   name: { english: string }
   type: string[]
 }
+
+// TODO: Move to some kind of configuration parameter
+const removeFairyType = true;
 
 export function findByName(name: string): Pokemon {
   const pokedexData = pokedexDataJSON as unknown as PokemonListJson
@@ -17,11 +20,19 @@ export function findByName(name: string): Pokemon {
   if (pokemonDexData === undefined) {
     throw new Error(`pokemon ${name} not found`)
   }
+
+  let type1 = pokemonDexData.type[0];
+  let type2 = pokemonDexData.type[1] ?? undefined;
+  if (removeFairyType) {
+    type1 = type1 === "Fairy" ? 'Normal' : type1;
+    type2 = (type2 ?? undefined) === "Fairy" ? undefined : type2;
+  }
+
   return {
     name: pokemonDexData.name.english,
     pokedexNumber: pokemonDexData.id,
-    type1: pokemonDexData.type[0],
-    type2: pokemonDexData.type[1] ?? undefined,
+    type1,
+    type2,
     spriteUrl: `${String(pokemonDexData.id).padStart(3, '0')}MS.png`,
   }
 }
