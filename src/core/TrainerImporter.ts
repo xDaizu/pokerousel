00e905709e18bda82fc426c94c60395ddc2018aa
Danny Even <danielparejom@gmail.com>
2024-09-19 17:40:16 +0200

    update trainers

--- src/core/TrainerImporter.ts
+++ src/core/TrainerImporter.ts
@@ -22,7 +22,7 @@ export function importTrainers(): Trainer[] {
  
   request.open("GET", "https://proxy.cors.sh/http://34.139.8.59:8080/pokerousel", false); // `false` makes the request synchronous
   request.setRequestHeader(
-    'x-cors-api-key', 'temp_3f1b708613dde738ad6c96ae2df3ca62'
+    'x-cors-api-key', 'temp_214a4bc422d4b8974a08de9c7f157e2a'
     );
   request.send(null);
 
